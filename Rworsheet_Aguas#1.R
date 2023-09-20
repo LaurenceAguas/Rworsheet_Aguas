40409f269b5145d2425b59665834967498ebd420
Laurence Aguas <laurence.aguas@students.isatu.edu.ph>
2023-09-20 03:58:03 +0000

    Activities

---
+++ Rworsheet_Aguas#1.R
@@ -0,0 +1,21 @@
+vector_age <- c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29, 35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41, 51, 35, 24, 33, 41)
+length (vector_age)
+
+reciprocal <- 1 / vector_age
+reciprocal
+
+new_age <- c(vector_age, 5,vector_age)
+new_age
+
+min(vector_age)
+max(vector_age)
+
+sort(new_age)
+
+vector_data <- c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3,
+                 2.5, 2.3, 2.4,2.7)
+vector_data
+
+new_vector_data <- c(vector_data, 2 ,vector_data)
+new_vector_data
+
